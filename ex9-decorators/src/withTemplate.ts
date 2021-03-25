49c0d0ab4aed9157d0e6c6679d72a27abced8640
L <mindOfL>
2021-03-25 18:22:27 +0100

    add decorator as a template

--- ex9-decorators/src/withTemplate.ts
+++ ex9-decorators/src/withTemplate.ts
@@ -1,5 +1,17 @@
-function WithTemplate() {
-
+function WithTemplate(template: string, hookId: string) {
+    // '_' indicates to JS that we don't need this argument inside function
+    return function(constructor: any) {
+        const personaje = new constructor()
+        document.getElementById(hookId)!.innerHTML = template
+        document.querySelector('h1')!.textContent = personaje.name
+    }
 }
 
-@WithTemplate
+@WithTemplate('<h1>This is a CustomPerson object</h1>', 'app')
+class CustomPerson {
+    name = 'L'
+
+    constructor() {
+        console.log('I\'m a person!!')
+    }
+}
