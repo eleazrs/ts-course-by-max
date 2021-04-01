3c7378a782d0f233a0e9323bce2f576fce98056c
L <mindOfL>
2021-04-01 19:09:56 +0200

    complete decorator types

--- ex9-decorators/src/logDecorator.ts
+++ ex9-decorators/src/logDecorator.ts
@@ -1,25 +1,43 @@
 // here we can see the Log Decorator functionality
 
+// property decorator, with defult parameters target & propertyName
 function Log(target: any, propertyName: string | Symbol) {
     console.log('Property decorator')
     console.log(target, propertyName)
 
 }
 
+// accessor decorator, with defult parameters target, name & descriptor
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
     console.log('Accessor decorator')
     console.log(target)
     console.log(name)
     console.log(descriptor)
+}
 
+// method decorator, with defult parameters target, name & descriptor
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator')
+    console.log(target)
+    console.log(name)
+    console.log(descriptor)
+}
+
+// parameter decorator, with default parameters target, name & position (the position as paremeter in the function calling, as index)
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator')
+    console.log(target)
+    console.log(name)
+    console.log(position)
 }
 
 class Product {
 
     @Log
     title: string
-    private _price: number
+    private _price: number // by convention use of '_' means that a property or method is private
     
+    @Log2
     set price(val: number) {
         if (val > 0)
         this._price = val
@@ -31,8 +49,9 @@ class Product {
         this.title = t
         this._price = p
     }
-
-    getPriceWithTax(tax: number) {
+    
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
         return this._price * (1 + (tax / 100))
     }
 }
