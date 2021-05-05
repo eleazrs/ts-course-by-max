9428f41b99f65fd04f5756cd51acbf579857899a
L <mindOfL>
2021-05-06 00:43:10 +0200

    finish autobind

--- ex9-decorators/src/autobind.ts
+++ ex9-decorators/src/autobind.ts
@@ -18,20 +18,31 @@ button.addEventListener('click', p.showMessage.bind(p))
 // can we build a thing to get autobind functionality? yes! with decorators!
 
 // declare decorator
-function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+function Autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
     // get the method to bind from annotated decorator trigger
     const originalMethod = descriptor.value
     const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false, //not an iterator ;)
         get() {
             // bind the method by its name
+            // 'this' will refer to whatever is responsible for triggering the scatter method
+            // ------------------
+            // the getter method will be triggered by the concrete object to which it belongs.
+            // it will always refer to the object on which we defined to gather.
+            // nothing will overwrite this binding because the getter is an extra layer between
+            // the function that's being executed and the object to which it belongs, and the event listener
             const boundFunction = originalMethod.bind(this)
+            return boundFunction
         }
     }
+    return adjDescriptor
 }
 
 class PrinterAutobinded {
     message = 'This works!'
 
+    @Autobind
     showMessage() {
         console.log(this.message)
     }
